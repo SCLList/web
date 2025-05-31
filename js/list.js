1ee9507a72ec72d38d25a43c75a934233fe12c50
SCLList <galaxystudiosmc@gmail.com>
2025-05-30 19:22:00 -0700

    Add files via upload

---
+++ js/list.js
@@ -0,0 +1,31 @@
+const d = {
+	"list": [
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "MichaelReeves",
+					"link": "https://youtu.be/IVUCnVHBPc8",
+					"percent": 100,
+					"hz": "288hz"
+				},
+			],
+			"name": "Grassy challen (grassy22)",
+                       "author": "GrassyXD",
+                       "more": "none",
+                       "id": 118517306,
+                       "pass": "N/A",
+                       "percentToQualify": 60,
+                       "verificationVid": "https://youtu.be/tw5wiX7GizA",
+                       "key": 0
+		},
+
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list;const version = d.version;
