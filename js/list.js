d9668c55a73cc6e619094870faf31cd6cb708d0b
SCLList <galaxystudiosmc@gmail.com>
2025-05-30 22:46:16 -0700

    Add files via upload

--- js/list.js
+++ js/list.js
@@ -19,7 +19,25 @@ const d = {
                        "verificationVid": "https://youtu.be/tw5wiX7GizA",
                        "key": 0
 		},
-
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "PLACEHOLDER",
+					"link": "https://youtu.be/VfvVwEKv7oQ",
+					"percent": 100,
+					"hz": "144hz"
+				},
+			],
+			"name": "Wave Challenge (grassy22)",
+                       "author": "GrassyXD",
+                       "more": "none",
+                       "id": 117828119,
+                       "pass": "N/A",
+                       "percentToQualify": 58,
+                       "verificationVid": "https://youtu.be/VfvVwEKv7oQ",
+                       "key": 0
+		},
 	],
 	"version": [
 		1,
