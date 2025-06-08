abde1978b609873995ff24b98d9e8dbede833ab3
SCLList <galaxystudiosmc@gmail.com>
2025-06-07 18:46:48 -0700

    Add files via upload

--- js/list.js
+++ js/list.js
@@ -1,6 +1,107 @@
 const d = {
 	"list": [
 		/*=================================================================================*/
+
+		{
+			"vids": [
+				{
+					"user": "faz4kongiro",
+					"link": "https://www.youtube.com/watch?v=VwqCp0c9oYU",
+					"percent": 100,
+					"hz": "60hz"
+				},
+				{
+					"user": "PLACEHOLDER",
+					"link": "https://youtu.be/VfvVwEKv7oQ",
+					"percent": 100,
+					"hz": "144hz"
+				},
+			],
+			"name": "Grassy challen (grassy22)",
+                       "author": "GrassyXD",
+                       "more": "none",
+                       "id": 118517306,
+                       "pass": "N/A",
+                       "percentToQualify": 60,
+                       "verificationVid": "https://youtu.be/tw5wiX7GizA",
+                       "key": 0
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "GrassyXD",
+					"link": "https://www.youtube.com/watch?v=_2SzVuauMkQ",
+					"percent": 100,
+					"hz": "144hz"
+				},
+				{
+					"user": "PLACEHOLDER",
+					"link": "https://youtu.be/VfvVwEKv7oQ",
+					"percent": 100,
+					"hz": "144hz"
+				},
+				{
+					"user": "LudkiGD",
+					"link": "https://youtu.be/nAI7GxoGAAI",
+					"percent": 100,
+					"hz": "144hz"
+				},
+			],
+			"name": "Leo Challen (Grassy2222)",
+                       "author": "faz4kongiro",
+                       "more": "none",
+                       "id": 120039772,
+                       "pass": "N/A",
+                       "percentToQualify": 55,
+                       "verificationVid": "https://www.youtube.com/watch?v=s74SWZSn8gU",
+                       "key": 1
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "GrassyXD",
+					"link": "https://youtu.be/oPO9DE1NZEs",
+					"percent": 100,
+					"hz": "144hz"
+				},
+				{
+					"user": "PLACEHOLDER",
+					"link": "https://youtu.be/_715tUA4wEY",
+					"percent": 100,
+					"hz": "60hz"
+				},
+			],
+			"name": "Zoquete challen 2 (ZoqueteRandom)",
+                       "author": "ZoqueteRandom",
+                       "more": "none",
+                       "id": 120174917,
+                       "pass": "N/A",
+                       "percentToQualify": 70,
+                       "verificationVid": "https://www.youtube.com/watch?v=TvFyMZZG49w&t=33s",
+                       "key": 2
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "PLACEHOLDER",
+					"link": "https://youtu.be/VfvVwEKv7oQ",
+					"percent": 100,
+					"hz": "144hz"
+				},
+			],
+			"name": "The Spikes Of Pain 3 (GrassyXD)",
+                       "author": "GrassyXD",
+                       "more": "none",
+                       "id": 120234849,
+                       "pass": "N/A",
+                       "percentToQualify": 60,
+                       "verificationVid": "https://youtu.be/uSGrkUYu4sA",
+                       "key": 3
+		},
+		/*=================================================================================*/
 		{
 			"vids": [
 				{
@@ -35,83 +136,9 @@ const d = {
                        "pass": "N/A",
                        "percentToQualify": 58,
                        "verificationVid": "https://youtu.be/I9xI-eR1LnA",
-                       "key": 0
+                       "key": 4
 		},
 		/*=================================================================================*/
-		{
-			"vids": [
-				{
-					"user": "faz4kongiro",
-					"link": "https://www.youtube.com/watch?v=VwqCp0c9oYU",
-					"percent": 100,
-					"hz": "60hz"
-				},
-				{
-					"user": "PLACEHOLDER",
-					"link": "https://youtu.be/VfvVwEKv7oQ",
-					"percent": 100,
-					"hz": "144hz"
-				},
-			],
-			"name": "Grassy challen (grassy22)",
-                       "author": "GrassyXD",
-                       "more": "none",
-                       "id": 118517306,
-                       "pass": "N/A",
-                       "percentToQualify": 60,
-                       "verificationVid": "https://youtu.be/tw5wiX7GizA",
-                       "key": 1
-		},
-		/*=================================================================================*/
-		{
-			"vids": [
-				{
-					"user": "PLACEHOLDER",
-					"link": "https://youtu.be/VfvVwEKv7oQ",
-					"percent": 100,
-					"hz": "144hz"
-				},
-			],
-			"name": "The Spikes Of Pain 3 (GrassyXD)",
-                       "author": "GrassyXD",
-                       "more": "none",
-                       "id": 120234849,
-                       "pass": "N/A",
-                       "percentToQualify": 60,
-                       "verificationVid": "https://youtu.be/uSGrkUYu4sA",
-                       "key": 2
-		},
-		/*=================================================================================*/
-		{
-			"vids": [
-				{
-					"user": "GrassyXD",
-					"link": "https://www.youtube.com/watch?v=_2SzVuauMkQ",
-					"percent": 100,
-					"hz": "144hz"
-				},
-				{
-					"user": "PLACEHOLDER",
-					"link": "https://youtu.be/VfvVwEKv7oQ",
-					"percent": 100,
-					"hz": "144hz"
-				},
-				{
-					"user": "LudkiGD",
-					"link": "https://youtu.be/nAI7GxoGAAI",
-					"percent": 100,
-					"hz": "144hz"
-				},
-			],
-			"name": "Leo Challen (Grassy2222)",
-                       "author": "faz4kongiro",
-                       "more": "none",
-                       "id": 119616687,
-                       "pass": "N/A",
-                       "percentToQualify": 55,
-                       "verificationVid": "https://www.youtube.com/watch?v=s74SWZSn8gU",
-                       "key": 3
-		},
 		{
 			"vids": [
 				{
@@ -134,31 +161,6 @@ const d = {
                        "pass": "N/A",
                        "percentToQualify": 55,
                        "verificationVid": "https://www.youtube.com/watch?v=CUr-CAenzzQ",
-                       "key": 4
-		},
-		/*=================================================================================*/
-		{
-			"vids": [
-				{
-					"user": "GrassyXD",
-					"link": "https://youtu.be/oPO9DE1NZEs",
-					"percent": 100,
-					"hz": "144hz"
-				},
-				{
-					"user": "PLACEHOLDER",
-					"link": "https://youtu.be/_715tUA4wEY",
-					"percent": 100,
-					"hz": "60hz"
-				},
-			],
-			"name": "Zoquete challen 2 (ZoqueteRandom)",
-                       "author": "ZoqueteRandom",
-                       "more": "none",
-                       "id": 120174917,
-                       "pass": "N/A",
-                       "percentToQualify": 70,
-                       "verificationVid": "https://www.youtube.com/watch?v=TvFyMZZG49w&t=33s",
                        "key": 5
 		},
 		/*=================================================================================*/
@@ -184,31 +186,6 @@ const d = {
 		{
 			"vids": [
 				{
-					"user": "PLACEHOLDER",
-					"link": "https://youtu.be/VfvVwEKv7oQ",
-					"percent": 100,
-					"hz": "144hz"
-				},
-				{
-					"user": "GrassyXD",
-					"link": "https://youtu.be/eNF-99CC-aw",
-					"percent": 100,
-					"hz": "144hz"
-				},
-			],
-			"name": "The Spikes Of Pain (faz4kongiro)",
-                       "author": "faz4kongiro",
-                       "more": "none",
-                       "id": 120042527,
-                       "pass": "N/A",
-                       "percentToQualify": 79,
-                       "verificationVid": "https://youtu.be/DAHYy9QoOOk",
-                       "key": 7
-		},
-		/*=================================================================================*/
-		{
-			"vids": [
-				{
 					"user": "faz4kongiro",
 					"link": "https://www.youtube.com/watch?v=R5DLgW6CTWo",
 					"percent": 100,
@@ -228,6 +205,31 @@ const d = {
                        "pass": "N/A",
                        "percentToQualify": 58,
                        "verificationVid": "https://youtu.be/VfvVwEKv7oQ",
+                       "key": 7
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "PLACEHOLDER",
+					"link": "https://youtu.be/VfvVwEKv7oQ",
+					"percent": 100,
+					"hz": "144hz"
+				},
+				{
+					"user": "GrassyXD",
+					"link": "https://youtu.be/eNF-99CC-aw",
+					"percent": 100,
+					"hz": "144hz"
+				},
+			],
+			"name": "The Spikes Of Pain (faz4kongiro)",
+                       "author": "faz4kongiro",
+                       "more": "none",
+                       "id": 120042527,
+                       "pass": "N/A",
+                       "percentToQualify": 79,
+                       "verificationVid": "https://youtu.be/DAHYy9QoOOk",
                        "key": 8
 		},
 		/*=================================================================================*/
